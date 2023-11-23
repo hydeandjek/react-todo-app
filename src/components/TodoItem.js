41287151b03b1cae5470de14c8dea381c8a7bde8
hydeandjek <hydeandjek@naver.com>
2023-11-23 13:02:14 +0900

    #2기본구조 및 scss

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MdDelete, MdDone } from 'react-icons/md';
-
+import './scss/TodoItem.scss';
 const TodoItem = () => {
   return (
     <li className='todo-list-item'>
--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TodoHeader from './TodoHeader';
 import TodoMain from './TodoMain';
 import TodoInput from './TodoInput';
+import './scss/TodoTemplate.scss';
 
 const TodoTemplate = () => {
   return (
