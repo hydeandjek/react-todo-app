02b77d9bbcbc06dca9bea472cecc65b8ef824df3
hydeandjek <hydeandjek@naver.com>
2023-11-24 13:16:36 +0900

    7. todo api와 todo app 연동

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -1,89 +1,100 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TodoHeader from './TodoHeader';
 import TodoMain from './TodoMain';
 import TodoInput from './TodoInput';
 import './scss/TodoTemplate.scss';
 
 const TodoTemplate = () => {
-  // 서버에 할 일 목록(json)을 요청 fetch해서 받아와야함. -> 나중에 하자
+  // 서버에 할 일 목록(json)을 요청(fetch)해서 받아와야 함.
+  const API_BASE_URL = 'http://localhost:8181/api/todos';
 
-  // todos 배열을 상태관리
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: '아침 산책하기',
-      done: true,
-    },
-    {
-      id: 2,
-      title: '오늘 주간 신문읽기',
-      done: true,
-    },
-    {
-      id: 3,
-      title: '샌드위치 사먹기',
-      done: false,
-    },
-    {
-      id: 4,
-      title: '리액트 복습하기',
-      done: false,
-    },
-  ]);
+  // todos 배열을 상태 관리
+  const [todos, setTodos] = useState([]);
 
-  // id 값 시퀀스 함수(DB 연동시키면 필요없게됨.)
-  const makeNewId = () => {
-    if (todos.length === 0) return 1;
-    return todos[todos.length - 1].id + 1; // 맨 마지막 할일 객체의 id보다 하나 크게
-  };
+  // id값 시퀀스 함수 (DB 연동시키면 필요없게 됨)
+  // const makeNewId = () => {
+  //   if (todos.length === 0) return 1;
+  //   return todos[todos.length - 1].id + 1; // 맨 마지막 할일 객체의 id보다 하나 크게
+  // };
 
   /*
     todoInput에게 todoText를 받아오는 함수
-    자식 컴포넌트가 부모 컴포넌트에게 데이터를 전달할 때는
-    일반적인 props 사용이 불가능.
+    자식 컴포넌트가 부모컴포넌트에게 데이터를 전달할 때는
+    일반적인 props 사용이 불가능
     부모 컴포넌트에서 함수를 선언(매개변수 꼭 선언) -> props로 함수를 전달
-    자식 컴포넌트에서 전달받은 함수를 호출하면서 매개값으로 데이터를 전달.  
+    자식 컴포넌트에서 전달받은 함수를 호출하면서 매개값으로 데이터를 전달.
   */
   const addTodo = (todoText) => {
     const newTodo = {
-      id: makeNewId(),
       title: todoText,
-      done: false,
-    }; // fetch 를 이용하여 백엔드에 insert 요청 보내야햄.
+    }; //fetch를 이용해서 백엔드 insert에 요청 보내야힘.
 
-    // todos.push(newTodo); 작동하지 않음 -> useState 변수는 setter로 변경
-    // setTodos(newTodo); 작동하지 않음
-    // react의 상태변수는 불변성 (immutable) 을 가지기 때문에
-    // 기존 상태에서 변경은 불가능 -> 새로운 상태로 만들어서 변경해야 한다.
+    // todos.push(newTodo); (x) -> useState 변수는 setter로 변경
+    // setTodos(todos.push(newTodo)); (x)
+    //react의 상태변수는 불변성(immutable)을 가지기 때문에
+    // 기본 상태에서 변경은 불가능 -> 새로운 상태로 만들어서 변경해야 한다.
+    // setTodos((oldTodos) => {
+    //   return [...oldTodos, newTodo];
+    // });
 
-    setTodos((oldTodos) => {
-      return [...oldTodos, newTodo];
-    });
+    fetch(API_BASE_URL, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(newTodo),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        setTodos(json.todos);
+      });
   };
 
   // 할 일 삭제 처리 함수
   const removeTodo = (id) => {
-    // 주어진 배열의 값들을 순회하여 조건에 맞는 요소들만 모아서 새로운 배열로 리턴
-    setTodos(todos.filter((todo) => todo.id !== id));
+    // 주어진 배열의 값들을 순회하여 조건에 맞는 요소들만 모아서 새로운 배열로 리턴.
+    // setTodos(todos.filter((todo) => todo.id !== id));
+
+    fetch(`${API_BASE_URL}/${id}`, {
+      method: 'DELETE',
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        setTodos(json.todos);
+      });
   };
 
   // 할 일 체크 처리 함수
-  const checkTodo = (id) => {
-    // const copyTodos = [...todos];
-    // for (let cTodo of copyTodos) {
-    //   if (cTodo.id === id) {
-    //     cTodo.done = !cTodo.done;
-    //   }
-    // }
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, done: !todo.done } : todo
-      )
-    );
+  const checkTodo = (id, done) => {
+    // setTodos(
+    //   todos.map((todo) =>
+    //     todo.id === id ? { ...todo, done: !todo.done } : todo
+    //   )
+    // );
+    fetch(API_BASE_URL, {
+      method: 'PATCH',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({
+        done: !done,
+        id: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((json) => setTodos(json.todos));
   };
 
-  // 체크가 안된 할일의 개수 카운트하기
+  // 체크가 안 된 할 일의 개수 카운트 하기
   const countRestTodo = () => todos.filter((todo) => !todo.done).length;
+
+  useEffect(() => {
+    // 페이지가 처음 렌더링 됨가 동시에 할 일 목록을 서버에 요청해서 뿌려 주겠습니다.
+    fetch(API_BASE_URL)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+
+        // fetch를 통해 받아온 데이터를 상태 변수에 할당.
+        setTodos(json.todos);
+      });
+  }, []);
 
   return (
     <div className='TodoTemplate'>
