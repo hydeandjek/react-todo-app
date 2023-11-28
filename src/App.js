e81cc409a2fc8cfc8255e03a966dc2ac8ebe417c
hydeandjek <hydeandjek@naver.com>
2023-11-28 14:30:13 +0900

    #10. 로그인 기능 구현(Context API)

--- src/App.js
+++ src/App.js
@@ -5,27 +5,33 @@ import Header from './components/layout/Header';
 import TodoTemplate from './components/todo/TodoTemplate';
 import Login from './components/user/Login';
 import Join from './components/user/Join';
+import { AuthContextProvider } from './utils/AuthContext';
 
 function App() {
   return (
-    <>
-      <Header />
-      <Routes>
-        <Route
-          path='/'
-          element={<TodoTemplate />}
-        />
-        <Route
-          path='/login'
-          element={<Login />}
-        />
-        <Route
-          path='/join'
-          element={<Join />}
-        />
-      </Routes>
-      <Footer />
-    </>
+    // 데이터를 전달하고자 하는 자식 컴포넌트들을 Provider 로 감쌉니다.
+    <AuthContextProvider>
+      <div className='wrapper'>
+        <Header />
+        <div className='content-wrapper'>
+          <Routes>
+            <Route
+              path='/'
+              element={<TodoTemplate />}
+            />
+            <Route
+              path='/login'
+              element={<Login />}
+            />
+            <Route
+              path='/join'
+              element={<Join />}
+            />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </AuthContextProvider>
   );
 }
 
